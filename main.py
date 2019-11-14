c58b3187b4546f5ecf20fdc4aa3191b5640bdbde
Gleb-create <57539172+Gleb-create@users.noreply.github.com>
2019-11-14 22:32:01 +0700

    Update main.py
    drawSquare function

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print ("hello world")
+import turtle
+from time import *
+
+
+def drawSquare(myTurtle, x, y, size):
+    myTurtle.penup()
+    myTurtle.goto(x, y)
+    myTurtle.left(90)
+    myTurtle.forward(size / 2)
+    myTurtle.pendown()
+    myTurtle.right(90)
+    myTurtle.forward(size/2)
+    myTurtle.right(90)
+    myTurtle.forward(size)
+    myTurtle.right(90)
+    myTurtle.forward(size)
+    myTurtle.right(90)
+    myTurtle.forward(size)
+    myTurtle.right(90)
+    myTurtle.forward(size/2)
+
+
+
+
+ada = turtle.Turtle()
+drawSquare(ada, 35, 35, 50)
+sleep(1000)
